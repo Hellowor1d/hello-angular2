bb9c8ccf7164019f078da67b53f54cddc449df53
HelloWor1d <1034597776@qq.com>
2017-01-22 11:50:38 +0800

    ng 命令可以快捷生成Component 模板

---
+++ hello-angular/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+//@Component是Angular提供的装饰器函数，用来描述Compoent的元数据
+//其中selector是指这个组件的在HTML模板中的标签是什么
+//template是嵌入（inline）的HTML模板，如果使用单独文件可用templateUrl
+//styles是嵌入（inline）的CSS样式，如果使用单独文件可用styleUrls
+@Component({
+  selector: 'app-login',
+  template: `
+    <p>
+      login Works!
+    </p>
+  `,
+  styles: []
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- hello-angular/src/main.ts
+++ hello-angular/src/main.ts
@@ -8,5 +8,15 @@ import { AppModule } from './app/app.module';
 if (environment.production) {
   enableProdMode();
 }
-
+//Angular编译器在浏览器中编译并引导该应用
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// AoT 预编译模式
+// // 不把编译器发布到浏览器
+// import { platformBrowser } from '@angular/platform-browser';
+
+// // 静态编译器会生成一个AppModule的工厂AppModuleNgFactory
+// import { AppModuleNgFactory } from './app.module.ngfactory';
+
+// // 引导AppModuleNgFactory
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
