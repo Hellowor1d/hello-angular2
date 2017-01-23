1cc8eadeb935bdcb47dfab3a3a8497c711f035cb
HelloWor1d <1034597776@qq.com>
2017-01-24 00:01:19 +0800

    添加todo的service

--- hello-angular/src/app/todo/todo.component.ts
+++ hello-angular/src/app/todo/todo.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 @Component({
   // 准确的说，moduleI指定，这是由于官方文档是使用System.js打包，而CLI是采用webpack打包造成的
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [ TodoService ]
 })
 export class TodoComponent implements OnInit {
 
   todos: Todo[] = [];
   desc = '';
 
-  constructor() { }
+  constructor(private service:TodoService) { }
 
   ngOnInit() {
   }
 
    addTodo(){
-    this.todos.push({id: 1, desc: this.desc, completed: false});
+    this.todos = this.service.addTodo(this.desc);
     this.desc = '';
   }
 
