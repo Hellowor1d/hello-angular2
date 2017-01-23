139799ceb540b324750f0feb36ebfe744345f033
HelloWor1d <1034597776@qq.com>
2017-01-23 22:00:07 +0800

    添加todo作为item的数据类型

--- hello-angular/src/app/todo/todo.component.ts
+++ hello-angular/src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // 准确的说，moduleI指定，这是由于官方文档是使用System.js打包，而CLI是采用webpack打包造成的
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
