3538057a283cd71f5e700eba4d5d8954c444c073
HelloWor1d <1034597776@qq.com>
2017-02-08 14:27:44 +0800

    设置好todo-item部分

--- hello-angular/src/app/todo/todo-item/todo-item.component.ts
+++ hello-angular/src/app/todo/todo-item/todo-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+//todo-item.component.ts
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent{
+  @Input() isChecked: boolean = false;
+  @Input() todoDesc: string = '';
+  @Output() onToggleTriggered = new EventEmitter<boolean>();
+  @Output() onRemoveTriggered = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit() {
+  toggle() {
+    this.onToggleTriggered.emit(true);
   }
-
+  remove() {
+    this.onRemoveTriggered.emit(true);
+  }
 }
